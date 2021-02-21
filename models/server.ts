099f5623296acdcb7c7a5d1f01d4d0b642510f5e
Diego Mauricio Viveros <diegocali114@gmail.com>
2021-02-21 14:38:09 -0500

    Server en TypeScript node y MySQL

---
+++ models/server.ts
@@ -0,0 +1,57 @@
+import express, { Application } from 'express';
+import userRoutes from '../routes/usuario';
+import cors from 'cors';
+
+import db from '../db/connection';
+
+class Server{
+
+    private app: Application;
+    private port: string;
+    private apiPaths = {
+        usuarios: '/api/usuarios'
+    }
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || '8000';
+
+        // Métodos iniciales
+        this.dbConnection();
+        this.middlewares();
+        this.routes();
+    }
+
+    async dbConnection() {
+        try {
+            await db.authenticate();
+            console.log("La base de datos es Online")
+        } catch (error) {
+            throw new Error( error );
+        }
+    }
+
+    middlewares() {
+        // CORS
+        this.app.use( cors() );
+
+        // Lectura del body
+        this.app.use( express.json() );
+
+        // Carpeta publica
+        this.app.use( express.static('public') );
+    }
+
+    routes() {
+        this.app.use( this.apiPaths.usuarios, userRoutes );
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en el puerto: ' + this.port );
+        })
+    }
+
+}
+
+export default Server;
